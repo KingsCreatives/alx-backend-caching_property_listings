1f218aad48a65beea8ec110798570cb9f29a4839
kwame cody <kwamecody@gmail.com>
2025-11-11 05:45:39 +0000

    cache property list view response in 15 minutes

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_redis',
+    'properties',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,7 @@ DATABASES = {
         'NAME': 'alx_cache_db',      
         'USER': 'alx_user',          
         'PASSWORD': 'alx_password',  
+        # 'HOST': '127.0.0.1',
         'HOST': 'postgres',          
         'PORT': '5432',
     }
@@ -88,6 +90,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://redis:6379/1", 
+        # "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all().order_by('-created_at')
+ 
+    context = {
+        'properties': properties,
+        'cached': True, 
+    }
+    return render(request, 'properties/property_list.html', context)
